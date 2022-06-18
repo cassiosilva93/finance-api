b4ff78aa6a229d6844df167f760430b64a26fe30
Cassio Oliveira Silva <cos@vortx.com.br>
2022-06-18 15:00:54 -0300

    chore: added test config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: false,
+  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+  collectCoverageFrom: [
+    '**/*.ts'
+  ]
+};
