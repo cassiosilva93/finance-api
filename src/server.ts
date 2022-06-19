6f198e16a3f44d41eec1231af7a55c51bc0f03c6
Cassio Oliveira Silva <cos@vortx.com.br>
2022-06-19 20:45:24 -0300

    chore(server): separate the server file

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,5 @@
-import { ApolloServer } from "apollo-server-express";
-import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
-import environmentVariables from './config/environment-variables';
-import schemas from "./graphql/schemas";
-import resolvers from "./graphql/resolvers";
-import express from "express";
-import http from "http";
-
-async function startApolloServer(typeDefs: any, resolvers: any) {
-  const { port, graphqlPath } = environmentVariables
-  const app = express();
-  const httpServer = http.createServer(app);
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  }) as any;
-  await server.start();
-  server.applyMiddleware({ app });
-  await new Promise<void>((resolve) =>
-    httpServer.listen({ port }, resolve)
-  );
-  console.log(`🚀 GraphQL API server running at http://localhost:${port}${graphqlPath}`);
-}
+import schemas from './graphql/schemas';
+import resolvers from './graphql/resolvers';
+import startApolloServer from './app';
 
 startApolloServer(schemas, resolvers);
