565f828516515b9c2f43a28587b58aa9311605b8
Cassio Oliveira Silva <cos@vortx.com.br>
2022-06-22 10:55:55 -0300

    feat(prisma): separeted prismaClient instance and added new migration to change type enum

---
+++ src/database/migrations/20220622134535_alter_transaction_type/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [DEPOSIT,WITHDRAW] on the enum `TransactionType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "TransactionType_new" AS ENUM ('income', 'outcome');
+ALTER TABLE "transactions" ALTER COLUMN "type" TYPE "TransactionType_new" USING ("type"::text::"TransactionType_new");
+ALTER TYPE "TransactionType" RENAME TO "TransactionType_old";
+ALTER TYPE "TransactionType_new" RENAME TO "TransactionType";
+DROP TYPE "TransactionType_old";
+COMMIT;
