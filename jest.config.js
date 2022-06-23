9fbf5dfa183687cb76d767c39ee04d15cd8cad06
Cassio Oliveira Silva <cos@vortx.com.br>
2022-06-23 08:55:54 -0300

    chore(configs): changed some confis

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: false,
-  testMatch: ["**/?(*.)+(spec|test).[jt]s?(x)"],
-  collectCoverageFrom: [
-    '**/*.ts'
-  ]
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  collectCoverageFrom: ['<rootDir>/src/domain/**/*.ts'],
 };
