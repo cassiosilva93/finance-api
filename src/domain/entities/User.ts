2e27803857d07b20c8fbcbc0eaa14e758b8a56c2
Cassio Oliveira Silva <cos@vortx.com.br>
2022-06-29 11:04:44 -0300

    chore(entity): add user entity

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,11 +1,18 @@
+import InvalidEmail from '../errors/InvalidEmail';
+import MaxLengthEmail from '../errors/MaxLengthEmail';
+import MinLengthPassword from '../errors/MinLengthPassword';
+import RequiredProperty from '../errors/RequiredProperty';
+import EmailEntity from './Email';
+import PasswordEntity from './Password';
+
 export default class User {
   public readonly id: string;
 
   public readonly name: string;
 
-  public readonly email: string;
+  public readonly email: EmailEntity;
 
-  public readonly password: string;
+  public readonly password: PasswordEntity;
 
   public readonly created_at: Date;
 
@@ -14,8 +21,8 @@ export default class User {
   constructor(
     id: string,
     name: string,
-    email: string,
-    password: string,
+    email: EmailEntity,
+    password: PasswordEntity,
     created_at: Date,
     updated_at: Date,
   ) {
@@ -25,5 +32,32 @@ export default class User {
     this.password = password;
     this.created_at = created_at;
     this.updated_at = updated_at;
+  }
+
+  static create(
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    created_at: Date,
+    updated_at: Date,
+  ):
+    | User
+    | RequiredProperty
+    | MinLengthPassword
+    | InvalidEmail
+    | MaxLengthEmail {
+    const emailOrError = EmailEntity.create(email);
+    const passwordOrError = PasswordEntity.create(password);
+    if (emailOrError instanceof Error) return emailOrError;
+    if (passwordOrError instanceof Error) return passwordOrError;
+    return new User(
+      id,
+      name,
+      emailOrError,
+      passwordOrError,
+      created_at,
+      updated_at,
+    );
   }
 }
