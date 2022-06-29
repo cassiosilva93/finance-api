559f99ba4a755b5b9a6de75a6f9bcda34f0f31c6
Cassio Oliveira Silva <cos@vortx.com.br>
2022-06-29 19:23:58 -0300

    refactor(user-dto): add undefined types in DTO

--- src/domain/dtos/User.ts
+++ src/domain/dtos/User.ts
@@ -1,9 +1,9 @@
 import EmailEntity from '../entities/Email';
 
 export interface UserDTO {
-  id: string;
-  name: string;
-  email: EmailEntity;
-  createdAt: Date;
-  updatedAt: Date;
+  id: string | undefined;
+  name: string | undefined;
+  email: EmailEntity | undefined;
+  created_at: Date | undefined;
+  updated_at: Date | undefined;
 }
