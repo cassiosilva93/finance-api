80264f89cd84c2b02ce9002e1a1e8ce941c1734c
Cassio Oliveira Silva <cos@vortx.com.br>
2022-06-30 17:59:22 -0300

    feat(login-usecase): add usecase to login

---
+++ src/domain/usecases/users/Login.ts
@@ -0,0 +1,28 @@
+import CryptographyAdapter from '@src/adapter/ports/Cryptography';
+import TokenAdapter from '@src/adapter/ports/Token';
+import config from '@src/config';
+import IncorrectEmailOrPassword from '@src/domain/errors/IncorrectEmailOrPassword';
+import UserRepository from '@src/domain/repositories/User';
+
+export default class Login {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly cryptography: CryptographyAdapter,
+    private readonly token: TokenAdapter,
+  ) {}
+
+  public async run(email: string, password: string) {
+    const user = await this.userRepository.findByEmail(email);
+    if (!user) return new IncorrectEmailOrPassword();
+    const isPasswordValid = await this.cryptography.compare(
+      password,
+      user.password.password,
+    );
+    if (!isPasswordValid) return new IncorrectEmailOrPassword();
+    const token = await this.token.encrypt(
+      { id: user.id },
+      config.jwt.expiresInDays,
+    );
+    return token;
+  }
+}
