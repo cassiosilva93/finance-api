86ad10d7d0c2efe97c9b154d8f7e189f352bcab7
Cassio Oliveira Silva <cos@vortx.com.br>
2022-07-01 18:45:07 -0300

    refactor(token-adapter): change token adapter path

--- src/domain/usecases/users/Login.ts
+++ src/domain/usecases/users/Login.ts
@@ -1,5 +1,5 @@
 import CryptographyAdapter from '@src/adapter/ports/Cryptography';
-import TokenAdapter from '@src/adapter/ports/Token';
+import TokenAdapter from '@src/adapter/ports/token/Token';
 import config from '@src/config';
 import IncorrectEmailOrPassword from '@src/domain/errors/IncorrectEmailOrPassword';
 import UserRepository from '@src/domain/repositories/User';
