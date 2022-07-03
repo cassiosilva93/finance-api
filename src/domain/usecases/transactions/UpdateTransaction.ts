a59f9d10816ddfe99489200787a435e9acb5b50f
Cassio Oliveira Silva <cos@vortx.com.br>
2022-07-03 19:16:47 -0300

    fix(update-transaction): fix usecase to update a transaction

--- src/domain/usecases/transactions/UpdateTransaction.ts
+++ src/domain/usecases/transactions/UpdateTransaction.ts
@@ -1,11 +1,29 @@
 import TransactionEntity from '@src/domain/entities/Transaction';
+import TransactionTypeEntity from '@src/domain/entities/TransactionType';
+import TransactionValueEntity from '@src/domain/entities/TransactionValue';
 import TransactionRepository from '@src/domain/repositories/Transaction';
 
 export default class UpdateTransaction {
   constructor(private readonly transactionRepository: TransactionRepository) {}
 
   public async run(id: string, data: TransactionEntity) {
-    const transaction = await this.transactionRepository.update(id, data);
+    let typeOrError;
+    let valueOrError;
+    let dataValidated = {
+      ...data,
+    } as any;
+    if (data.type)
+      typeOrError = TransactionTypeEntity.create(String(data.type));
+    if (typeOrError instanceof Error) return typeOrError;
+    if (data.value || Number(data.value) >= 0)
+      valueOrError = TransactionValueEntity.create(Number(data.value));
+    if (valueOrError instanceof Error) return valueOrError;
+    if (typeOrError) dataValidated.type = typeOrError.type;
+    if (valueOrError) dataValidated.value = valueOrError.value;
+    const transaction = await this.transactionRepository.update(
+      id,
+      dataValidated,
+    );
     return transaction;
   }
 }
