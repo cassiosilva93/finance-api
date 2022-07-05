8894a2809fdc52c6c3189e6d90e52c874f16a6d0
Cassio Oliveira Silva <cos@vortx.com.br>
2022-07-05 15:31:57 -0300

    task/change-server

--- src/config/graphql.ts
+++ src/config/graphql.ts
@@ -1,5 +1,6 @@
 const graphql = {
   port: process.env.API_GRAPHQL_PORT || 3000,
+  path: process.env.API_GRAPHQL_PATH || 'graphql',
 };
 
 export default graphql;
--- src/infra/graphql/server/index.ts
+++
@@ -1,5 +0,0 @@
-import resolvers from '../resolvers';
-import schemas from '../schemas';
-import startApolloServer from './app';
-
-startApolloServer(schemas, resolvers);
