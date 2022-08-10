7f98da65d30d7d194d793f5bd5d1581c59aecd2c
Cassio Oliveira Silva <cos@vortx.com.br>
2022-08-10 19:06:18 -0300

    refactor: change HttpResponse constructor to sintax sugar

--- src/adapter/ports/httpResponse/HttpResponse.ts
+++ src/adapter/ports/httpResponse/HttpResponse.ts
@@ -1,12 +1,5 @@
 class HttpResponse {
-  public statusCode: number;
-
-  public body: unknown;
-
-  constructor(statusCode: number, body: unknown) {
-    this.statusCode = statusCode;
-    this.body = body;
-  }
+  constructor(readonly statusCode: number, readonly body: unknown) {}
 }
 
 export default HttpResponse;
